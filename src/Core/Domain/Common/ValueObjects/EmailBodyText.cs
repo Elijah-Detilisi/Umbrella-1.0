a9f3a2dcd4b052c3cf7e16776ff4906de9f516f0
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-10-11 20:15:38 +0200

    Added EmailBodyText and unit tests

---
+++ src/Core/Domain/Common/ValueObjects/EmailBodyText.cs
@@ -0,0 +1,37 @@
+﻿namespace Domain.Common.ValueObjects;
+
+public class EmailBodyText : ValueObject
+{
+    private const int MaxBodyLength = 1000;
+    private string Value { get; }
+
+    private EmailBodyText(string value)
+    {
+        Value = value;
+    }
+
+    public static EmailBodyText Create(string body)
+    {
+        if (string.IsNullOrWhiteSpace(body))
+        {
+            throw new ArgumentException("Email body content cannot be empty or null.");
+        }
+
+        if (body.Length > MaxBodyLength)
+        {
+            throw new ArgumentException($"Email body content exceeds the maximum length of {MaxBodyLength} characters.");
+        }
+
+        return new EmailBodyText(body);
+    }
+
+    public override string ToString()
+    {
+        return Value;
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
