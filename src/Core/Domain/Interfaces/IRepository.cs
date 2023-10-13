248d933f6f5221cf4f87c35ea65725c67d624762
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-10-14 01:43:37 +0200

    Create IRepository.cs

---
+++ src/Core/Domain/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq.Expressions;
+
+namespace Domain.Interfaces;
+
+public interface IRepository<TEntity> where TEntity : Entity
+{
+    void Save(TEntity entity);
+    void Remove(TEntity entity);
+    TEntity FindById(Guid id);
+    IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> spec);
+}
