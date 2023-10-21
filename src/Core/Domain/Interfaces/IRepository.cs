9a46733786297479e5d39958ef42148377a48f59
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-10-21 14:16:42 +0200

    Update IRepository.cs

--- src/Core/Domain/Interfaces/IRepository.cs
+++ src/Core/Domain/Interfaces/IRepository.cs
@@ -2,11 +2,11 @@
 
 public interface IRepository<T> where T : Entity
 {
-    void Save();
-    Task<int> Add(T entity);
-    Task<int> Update(T entity);
-    Task<int> Delete(T entity);
-    Task<T> GetById(Guid id);
+    T GetById(Guid id);
+    void Add(T entity);
+    void Remove(T entity);
+    void Update(T entity);
+    IEnumerable<T> List();
     Task<IEnumerable<T>> Get(Expression<Func<T, bool>> spec);
 
 }  
