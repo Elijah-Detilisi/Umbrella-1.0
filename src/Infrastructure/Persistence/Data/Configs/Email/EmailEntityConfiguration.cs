b560d5a869a46b2454e761b8afd16f86722d4b83
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-10-28 00:01:39 +0200

    Create EmailEntityConfiguration.cs

---
+++ src/Infrastructure/Persistence/Data/Configs/Email/EmailEntityConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using Domain.Email.ValueObjects;
+using System.Text.Json;
+
+namespace Persistence.Data.Configs.Email;
+
+public class EmailEntityConfiguration : IEntityTypeConfiguration<EmailEntity>
+{
+    public void Configure(EntityTypeBuilder<EmailEntity> builder)
+    {
+        builder.ToTable(nameof(EmailEntity));
+        builder.Property(e => e.Type).IsRequired();
+        builder.Property(e => e.EmailStatus).IsRequired();
+        builder.Property(e => e.CreatedAt).HasDefaultValueSql("GETUTCDATE()");
+
+        ConfigureValueObjects(builder);
+    }
+
+    private void ConfigureValueObjects(EntityTypeBuilder<EmailEntity> builder)
+    {
+        builder.OwnsOne(
+            e => e.Body, bodyBuilder =>
+            {
+                bodyBuilder.Property(body => body.Value)
+                    .HasColumnName("Body").HasMaxLength(EmailBodyText.MAXBODYLENGTH);
+            }
+        );
+
+        builder.OwnsOne(
+            e => e.Subject, subjectBuilder =>
+            {
+                subjectBuilder.Property(subject => subject.Value)
+                    .HasColumnName("Subject").HasMaxLength(EmailSubjectLine.MAXSUBJECTLINELENGTH);
+            }
+        );
+
+        var jsonOption = new JsonSerializerOptions();
+        builder.Property(e => e.Recipients)
+            .HasColumnName("Recipients")
+            .HasConversion(
+                v => JsonSerializer.Serialize(v, jsonOption),
+                v => JsonSerializer.Deserialize<List<EmailAddress>>(v, jsonOption)
+        ); 
+
+
+    }
+}
