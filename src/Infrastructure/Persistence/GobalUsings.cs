db81b05fd2bf2f255d64c1506b47ecbdb8eab0aa
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-10-27 22:54:46 +0200

    Update GobalUsings.cs

--- src/Infrastructure/Persistence/GobalUsings.cs
+++ src/Infrastructure/Persistence/GobalUsings.cs
@@ -1,3 +1,5 @@
-﻿global using Domain.Email.Entities;
+﻿global using Domain.Common.ValueObjects;
+global using Domain.Email.Entities;
 global using Domain.User.Entities;
 global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
