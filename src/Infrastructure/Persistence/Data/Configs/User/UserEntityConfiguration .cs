8e29daaca98f2a6eb861e39c1811501525ddab68
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-10-28 00:01:42 +0200

    Create UserEntityConfiguration .cs

---
+++ src/Infrastructure/Persistence/Data/Configs/User/UserEntityConfiguration .cs
@@ -0,0 +1,34 @@
+﻿using Domain.User.ValueObjects;
+
+namespace Persistence.Data.Configs.User;
+
+public class UserConfigurations : IEntityTypeConfiguration<UserEntity>
+{
+    public void Configure(EntityTypeBuilder<UserEntity> builder)
+    {
+        builder.ToTable(nameof(UserEntity));
+        builder.Property(e => e.UserName).IsRequired();
+        builder.Property(e => e.CreatedAt).HasDefaultValueSql("GETUTCDATE()"); 
+
+        ConfigureValueObjects(builder);
+    }
+
+    private void ConfigureValueObjects(EntityTypeBuilder<UserEntity> builder)
+    {
+        builder.OwnsOne(
+            e => e.EmailAddress, emailBuilder =>
+            {
+                emailBuilder.Property(email => email.Value)
+                    .HasColumnName(nameof(EmailAddress));
+            }
+        );
+
+        builder.OwnsOne(
+            e => e.EmailPassword, passwordBuilder =>
+            {
+                passwordBuilder.Property(password => password.Value)
+                    .HasColumnName(nameof(EmailPassword));
+            }
+        );
+    }
+}
