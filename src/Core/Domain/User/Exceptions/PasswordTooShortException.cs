ed4a12a22e67af6c0073f852fb6c4eda96d4eb4f
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-10-27 22:40:57 +0200

    Create PasswordTooShortException.cs

---
+++ src/Core/Domain/User/Exceptions/PasswordTooShortException.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.User.Exceptions;
+
+public class PasswordTooShortException: Exception
+{
+    public PasswordTooShortException(int limitValue) :
+        base($"Password must be at least {limitValue} characters long.")
+    {
+    }
+}
