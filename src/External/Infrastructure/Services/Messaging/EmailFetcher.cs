84c2b167fb1634d8c7c0719161754397c356e954
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-11-11 00:01:28 +0200

    Add email fetcher implementation

--- src/External/Infrastructure/Services/Messaging/EmailFetcher.cs
+++ src/External/Infrastructure/Services/Messaging/EmailFetcher.cs
@@ -1,5 +1,38 @@
-﻿namespace Infrastructure.Services.Messaging;
+﻿using Domain.User.Entities;
+
+namespace Infrastructure.Services.Messaging;
 
 public class EmailFetcher
 {
+    private readonly int _portNumber;
+    private readonly string _serverName;
+    private readonly UserEntity _userEntity;
+
+    public EmailFetcher(UserEntity userEntity, string serverName, int portNumer)
+    {
+        _portNumber = portNumer;
+        _serverName = serverName;
+        _userEntity = userEntity;
+    }
+
+    public async Task<List<MimeMessage>> GetEmailsAsync()
+    {
+        var allMessages = new List<MimeMessage>();
+
+        using (var client = new Pop3Client())
+        {
+            await client.ConnectAsync(_serverName, _portNumber, true);
+            await client.AuthenticateAsync(_userEntity.EmailAddress.ToString(), _userEntity.EmailPassword.ToString());
+
+            for (int i = 0; i < client.Count; i++)
+            {
+                var mimeMessage = client.GetMessage(i);
+                allMessages.Add(mimeMessage);
+            }
+
+            await client.DisconnectAsync(true);
+        }
+
+        return allMessages;
+    }
 }
