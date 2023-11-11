e1d71689c63edeb6bd393b29e87b181498148f53
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-11-11 15:19:34 +0200

    Update EmailSender.cs

--- src/External/Infrastructure/Services/Messaging/EmailSender.cs
+++ src/External/Infrastructure/Services/Messaging/EmailSender.cs
@@ -1,7 +1,34 @@
-﻿using MimeKit;
+﻿using Domain.User.Entities;
+using MimeKit;
 
 namespace Infrastructure.Services.Messaging;
 
 public class EmailSender
 {
+    private readonly int _portNumber;
+    private readonly string _serverName;
+    private readonly UserEntity _userEntity;
+
+    public EmailSender(UserEntity userEntity, string serverName, int portNumer)
+    {
+        _portNumber = portNumer;
+        _serverName = serverName;
+        _userEntity = userEntity;
+    }
+
+    public async Task SendEmailAsync(MimeMessage message)
+    {
+        using (var client = new SmtpClient())
+        {
+            await client.ConnectAsync(_serverName, _portNumber, true);
+            await client.AuthenticateAsync(
+                _userEntity.EmailAddress.ToString(), 
+                _userEntity.EmailPassword.ToString()
+            );
+
+            await client.SendAsync(message);
+            await client.DisconnectAsync(true);
+        }
+    }
+
 }
