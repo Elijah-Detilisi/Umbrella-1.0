99fe8e665f58280d8d27c53b8275813c15b7c916
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-11-11 14:28:36 +0200

    Create EmailServerSetting.cs

---
+++ src/Core/Domain/Common/ValueObjects/EmailServerSetting.cs
@@ -0,0 +1,28 @@
+﻿namespace Domain.Common.ValueObjects;
+
+public class EmailServerSetting : ValueObject<Tuple<int, string>>
+{
+    public EmailServerSetting(Tuple<int, string> value) : base(value)
+    {
+    }
+
+    public static EmailServerSetting Create(int portNumber, string serverName)
+    {
+        if (string.IsNullOrWhiteSpace(serverName))
+        {
+            throw new EmptyValueException(nameof(serverName));
+        }
+
+        if (portNumber <= 0)
+        {
+            throw new ArgumentException("Port number must be greater than zero.", nameof(portNumber));
+        }
+
+        return new EmailServerSetting(Tuple.Create(portNumber, serverName));
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
