f6d920bd5bc5bdb421ea6ec3abb23d2584cd6da9
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-11-18 17:54:47 +0200

    Add presentation layer

---
+++ src/Presentation/Umbrella.App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Umbrella.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/Presentation/Umbrella.App/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Umbrella.App
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
