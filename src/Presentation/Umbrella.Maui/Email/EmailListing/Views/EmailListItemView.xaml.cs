8358b99bff362c64d141deee40046044f9a5c2ac
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-12-05 22:04:31 +0200

    Update EmailListItemView.xaml.cs

--- src/Presentation/Umbrella.Maui/Email/EmailListing/Views/EmailListItemView.xaml.cs
+++ src/Presentation/Umbrella.Maui/Email/EmailListing/Views/EmailListItemView.xaml.cs
@@ -1,3 +1,7 @@
+using Application.Email.Models;
+using Domain.Common.ValueObjects;
+using Domain.Email.ValueObjects;
+
 namespace Umbrella.Maui.Email.EmailListing.Views;
 
 public partial class EmailListItemView : ContentView
@@ -6,4 +10,22 @@ public partial class EmailListItemView : ContentView
 	{
 		InitializeComponent();
 	}
+
+    public EmailModel EmailMessage
+    {
+        get => (EmailModel)GetValue(EmailMessageProperty);
+        set => SetValue(EmailMessageProperty, value);
+    }
+    
+    public static readonly BindableProperty EmailMessageProperty = BindableProperty.Create(
+        propertyName: nameof(EmailMessage),
+        returnType: typeof(EmailModel),
+        declaringType: typeof(EmailListItemView),
+        defaultBindingMode: BindingMode.TwoWay,
+        defaultValue: new EmailModel()
+        {
+            Sender = EmailAddress.Create("default@umbrella.com"),
+            Subject = EmailSubjectLine.Create("Welcome to Umbrella - Your Ultimate Email Companion!")
+        }
+    );
 }
