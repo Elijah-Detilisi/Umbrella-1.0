3b4546a8d730d371451198efbe8cc7d2bbd5be49
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-12-10 23:27:58 +0200

    Update SpeechBubble.cs

--- src/Presentation/Umbrella.Maui/Email/Base/Views/SpeechBubble.cs
+++ src/Presentation/Umbrella.Maui/Email/Base/Views/SpeechBubble.cs
@@ -1,33 +1,31 @@
-﻿using Microsoft.Maui.Controls.Shapes;
-
-namespace Umbrella.Maui.Email.Base.Views;
+﻿namespace Umbrella.Maui.Email.Base.Views;
 
 public class SpeechBubble : Grid
 {
-    public SpeechBubble()
+    public SpeechBubble(bool isBotSpeaker)
     {
-        InitializeLayout();
-        InitializeChildern();
+        InitializeLayout(isBotSpeaker);
+        InitializeChildern(isBotSpeaker);
     }
 
-    private void InitializeLayout()
+    private void InitializeLayout(bool isBotSpeaker)
     {
         Padding = new Thickness(10);
         ColumnDefinitions = new ColumnDefinitionCollection
         {
-            new ColumnDefinition { Width = new GridLength(0.2, GridUnitType.Star) },
-            new ColumnDefinition { Width = new GridLength(0.8, GridUnitType.Star) }
+            new ColumnDefinition { Width = new GridLength( isBotSpeaker? 0.2 : 0.8, GridUnitType.Star) },
+            new ColumnDefinition { Width = new GridLength(isBotSpeaker? 0.8 : 0.2, GridUnitType.Star) }
         };
     }
 
-    private void InitializeChildern()
+    private void InitializeChildern(bool isBotSpeaker)
     {
         //Speaker icon
         Children.Add(new Image
         {
             Source = "user_solid.svg",
             WidthRequest = 30, HeightRequest = 30,
-        }.Column(0)
+        }.Column(isBotSpeaker? 0 : 1)
         );
 
         //Message box
@@ -37,7 +35,7 @@ public class SpeechBubble : Grid
             {
                 Text = "Hello! This is a speech bubble with an image.",
             }
-        }.DynamicResource(View.StyleProperty, "FrameSpeechBox").Column(1)
+        }.DynamicResource(View.StyleProperty, "FrameSpeechBox").Column(isBotSpeaker ? 1 : 0)
         );;
     }
 }
