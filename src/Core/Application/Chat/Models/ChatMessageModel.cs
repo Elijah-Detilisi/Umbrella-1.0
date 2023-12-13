95f0a564be7dfdf92a6a349c4b99757771653d57
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-12-13 19:15:57 +0200

    Create ChatMessageModel.cs

---
+++ src/Core/Application/Chat/Models/ChatMessageModel.cs
@@ -0,0 +1,10 @@
+﻿using Application.Chat.Enums;
+
+namespace Application.Chat.Models;
+
+public class ChatMessageModel : Model
+{
+    public ChatSender Sender { get; set; }
+    public string Message { get; set; }
+    public DateTime Timestamp => CreatedAt;
+}
