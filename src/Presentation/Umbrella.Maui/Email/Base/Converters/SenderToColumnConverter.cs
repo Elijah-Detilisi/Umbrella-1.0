3aa0f61df01a916e7163fee596a58a62ac1446c4
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-12-15 01:43:02 +0200

    Create SenderToColumnConverter.cs

---
+++ src/Presentation/Umbrella.Maui/Email/Base/Converters/SenderToColumnConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace Umbrella.Maui.Email.Base.Converters;
+
+public class SenderToColumnConverter : IValueConverter
+{
+    private enum Column { Left = 0, Right = 1 }
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is ChatSender sender)
+        {
+            return sender == ChatSender.Human ? (int)Column.Right : (int)Column.Left;
+        }
+
+        return (int)Column.Left; // Default to Left column
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
+
