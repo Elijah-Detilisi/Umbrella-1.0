ed3e3ebcd53edaea3c748c2af43b8466bfea60c4
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-12-16 11:30:02 +0200

    Update IMauiSpeechRecognition.cs

--- src/Core/Application/Common/Services/IMauiSpeechRecognition.cs
+++ src/Core/Application/Common/Services/IMauiSpeechRecognition.cs
@@ -5,5 +5,6 @@ namespace Application.Common.Services;
 public interface IMauiSpeechRecognition
 {
     ValueTask DisposeAsync();
+    Task<bool> RequestPermissions(CancellationToken cancellationToken);
     Task<string> Listen(CultureInfo culture, IProgress<string> recognitionResult, CancellationToken cancellationToken);
 }
