ebdc158d2f38abf79c8cd0bc6c2ae609639ec429
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-12-16 17:17:50 +0200

    Add speechRecognition implementation

--- src/Core/Application/Common/Services/IAppSpeechRecognition.cs
+++ src/Core/Application/Common/Services/IAppSpeechRecognition.cs
@@ -6,5 +6,5 @@ public interface IAppSpeechRecognition : IAsyncDisposable
 {
     Task<bool> RequestPermissions(CancellationToken cancellationToken = default);
     Task StopListenAsync(CancellationToken cancellationToken = default);
-    Task<string> ListenAsync(CultureInfo culture, IProgress<string>? recognitionResult, CancellationToken cancellationToken = default);
+    Task ListenAsync(IProgress<string>? recognitionResult, CancellationToken cancellationToken = default);
 }
---
+++ src/External/Infrastructure/AppSpeechRecognition.cs
@@ -0,0 +1,41 @@
+﻿using Application.Common.Services;
+using CommunityToolkit.Maui.Media;
+using System.Globalization;
+
+namespace Infrastructure;
+
+// All the code in this file is included in all platforms.
+public class AppSpeechRecognition : IAppSpeechRecognition
+{
+    //Fields
+    private readonly ISpeechToText speechToText;
+    private const string defaultLanguage = "en-US";
+    
+    //Construction
+    public AppSpeechRecognition()
+    {
+        speechToText = SpeechToText.Default;
+    }
+
+    //Permision method
+    public Task<bool> RequestPermissions(CancellationToken cancellationToken = default)
+    {
+        return speechToText.RequestPermissions(cancellationToken);
+    }
+
+    //Listen method
+    public async Task ListenAsync(IProgress<string>? recognitionResult, CancellationToken cancellationToken = default)
+    {
+        await speechToText.ListenAsync(CultureInfo.GetCultureInfo(defaultLanguage), recognitionResult, cancellationToken);
+    }
+
+    public Task StopListenAsync(CancellationToken cancellationToken = default)
+    {
+        return speechToText.StopListenAsync(cancellationToken);
+    }
+
+    public ValueTask DisposeAsync()
+    {
+        return speechToText.DisposeAsync();
+    }
+}
