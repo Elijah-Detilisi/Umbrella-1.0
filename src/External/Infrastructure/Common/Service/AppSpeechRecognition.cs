26b1ab28a73db69140b918202f2b4fd671a40dfe
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-12-16 19:13:01 +0200

    Create AppSpeechRecognition.cs

---
+++ src/External/Infrastructure/Common/Service/AppSpeechRecognition.cs
@@ -0,0 +1,38 @@
+﻿namespace Infrastructure.Common.Service;
+
+public class AppSpeechRecognition : IAppSpeechRecognition
+{
+    //Fields
+    private readonly ISpeechToText speechToText;
+    private const string defaultLanguage = "en-US";
+
+    //Construction
+    public AppSpeechRecognition()
+    {
+        speechToText = SpeechToText.Default;
+    }
+
+    //Permision method
+    public Task<bool> RequestPermissions(CancellationToken cancellationToken = default)
+    {
+        return speechToText.RequestPermissions(cancellationToken);
+    }
+
+    //Listen method
+    public async Task ListenAsync(IProgress<string>? recognitionResult, CancellationToken cancellationToken = default)
+    {
+        await speechToText.ListenAsync(CultureInfo.GetCultureInfo(defaultLanguage), recognitionResult, cancellationToken);
+    }
+
+    //Stop method
+    public Task StopListenAsync(CancellationToken cancellationToken = default)
+    {
+        return speechToText.StopListenAsync(cancellationToken);
+    }
+
+    //Dispose method
+    public ValueTask DisposeAsync()
+    {
+        return speechToText.DisposeAsync();
+    }
+}
