f2c0aeaccf771994aadcd5a8b752a2cca85948b8
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-12-16 13:03:22 +0200

    Update ChatViewModel.cs

--- src/Core/Application/Chat/ViewModels/ChatViewModel.cs
+++ src/Core/Application/Chat/ViewModels/ChatViewModel.cs
@@ -1,12 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Application.Common.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Diagnostics;
+using System.Globalization;
 
 namespace Application.Chat.ViewModels
 {
-    internal class ChatViewModel
+    public partial class ChatViewModel : ViewModel
     {
+        //Service
+        private readonly IMauiSpeechRecognition _speechRecognition;
+
+        //Construction
+        public ChatViewModel(IMauiSpeechRecognition speechRecognition)
+        {
+            _speechRecognition = speechRecognition;
+        }
+
+        //Properties
+        [ObservableProperty]
+        public string recognitionText = string.Empty;
+
+        [ObservableProperty, NotifyCanExecuteChangedFor(nameof(ListenCommand))]
+        public bool canListenExecute = true;
+
+        [ObservableProperty, NotifyCanExecuteChangedFor(nameof(StopListenCommand))]
+        bool canStopListenExecute = false;
+
+        //Commands
+        [RelayCommand(IncludeCancelCommand = true, CanExecute = nameof(CanListenExecute))]
+        public async Task Listen(CancellationToken cancellationToken)
+        {
+            try
+            {
+                CanStopListenExecute = true;
+                var isGranted = await _speechRecognition.RequestPermissions(cancellationToken);
+                if (!isGranted)
+                {
+                    Debug.WriteLine("Permission not granted");
+                    return;
+                }
+                var beginSpeakingPrompt = "Begin speaking...";
+                RecognitionText = beginSpeakingPrompt;
+
+                RecognitionText = await _speechRecognition.Listen(CultureInfo.GetCultureInfo("en-us"),
+                    new Progress<string>(partialText =>
+                    {
+                        if (RecognitionText == beginSpeakingPrompt)
+                        {
+                            RecognitionText = string.Empty;
+                        }
+
+                        RecognitionText += partialText + " ";
+                    }), cancellationToken);
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                Debug.WriteLine("You said: " + RecognitionText);
+            }
+        }
+
+        [RelayCommand(CanExecute = nameof(CanStopListenExecute))]
+        public void StopListen(CancellationToken cancellationToken)
+        {
+            CanListenExecute = true;
+            CanStopListenExecute = false;
+        }
     }
 }
