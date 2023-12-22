3ad2f9444a4b3cd796430f852b561e0bf615f7b5
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-12-22 21:16:08 +0200

    Create ChatMessageModelTests.cs

---
+++ tests/Application.UnitTests/ChatTests/ModelTests/ChatMessageModelTests.cs
@@ -0,0 +1,64 @@
+﻿using Application.Chat.Enums;
+using Application.Chat.Models;
+
+namespace Application.UnitTests.ChatTests.ModelTests;
+
+[TestFixture]
+public class ChatMessageModelTests
+{
+    [Test]
+    public void ChatMessageModel_CreatedAt_ShouldBeSet()
+    {
+        // Arrange
+        ChatMessageModel chatMessage = new();
+
+        // Assert
+        Assert.That(chatMessage.CreatedAt, Is.Not.EqualTo(default(DateTime)));
+    }
+
+    [Test]
+    public void ChatMessageModel_ModifiedAt_ShouldBeSet()
+    {
+        // Arrange
+        ChatMessageModel chatMessage = new();
+
+        // Assert
+        Assert.That(chatMessage.ModifiedAt, Is.Not.EqualTo(default(DateTime)));
+    }
+
+    [Test]
+    public void ChatMessageModel_Sender_ShouldBeSet()
+    {
+        // Arrange
+        ChatMessageModel chatMessage = new()
+        {
+            Sender = ChatSender.Human
+        };
+
+        // Assert
+        Assert.That(chatMessage.Sender, Is.EqualTo(ChatSender.Human));
+    }
+
+    [Test]
+    public void ChatMessageModel_Message_ShouldBeSet()
+    {
+        // Arrange
+        ChatMessageModel chatMessage = new()
+        {
+            Message = "Hello, World!"
+        };
+
+        // Assert
+        Assert.That(chatMessage.Message, Is.EqualTo("Hello, World!"));
+    }
+
+    [Test]
+    public void ChatMessageModel_Timestamp_ShouldBeEqualToCreatedAt()
+    {
+        // Arrange
+        ChatMessageModel chatMessage = new();
+
+        // Assert
+        Assert.That(chatMessage.Timestamp, Is.EqualTo(chatMessage.CreatedAt));
+    }
+}
