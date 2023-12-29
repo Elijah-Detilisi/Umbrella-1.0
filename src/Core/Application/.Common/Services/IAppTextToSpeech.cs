b6912ef64f6869c4aab75818a18d734740d00da0
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-12-29 20:52:33 +0200

    Update IAppTextToSpeech.cs

--- src/Core/Application/.Common/Services/IAppTextToSpeech.cs
+++ src/Core/Application/.Common/Services/IAppTextToSpeech.cs
@@ -2,6 +2,11 @@
 
 public interface IAppTextToSpeech
 {
-    Task SetSpeechOptions(float pitch, float volume);
+    //Properties
+    public float Pitch { get; }
+    public float Volume { get; }
+
+    //Methods
+    void SetSpeechOptions(float pitch, float volume);
     Task SpeakAsync(string text, CancellationToken cancelToken = default);
 }
