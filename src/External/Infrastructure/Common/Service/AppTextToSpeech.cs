047c05fb217a8a09626857b45f4285c518dc6a5e
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-12-29 20:52:37 +0200

    Create AppTextToSpeech.cs

---
+++ src/External/Infrastructure/Common/Service/AppTextToSpeech.cs
@@ -0,0 +1,36 @@
+﻿namespace Infrastructure.Common.Service;
+
+public class AppTextToSpeech : IAppTextToSpeech
+{
+    //Fields
+    private readonly ITextToSpeech textToSpeech;
+
+    //Properties
+    public float Pitch { get; private set; }
+    public float Volume { get; private set; }
+    
+    //Construction
+    public AppTextToSpeech()
+    {
+        textToSpeech = TextToSpeech.Default;
+    }
+
+    //Options method
+    public void SetSpeechOptions(float pitch, float volume)
+    {
+        Pitch = pitch;
+        Volume = volume;
+    }
+
+    //Speak method
+    public async Task SpeakAsync(string text, CancellationToken cancelToken = default)
+    {
+        var options = new SpeechOptions()
+        {
+            Pitch = this.Pitch,
+            Volume = this.Volume,
+        };
+
+        await textToSpeech.SpeakAsync(text, options, cancelToken);
+    }
+}
