4271be032785031749a9ccaea86c50052703dca8
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-12-31 12:36:04 +0200

    Create EmailViewModel.cs

---
+++ src/Core/Application/Email/.Base/EmailViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Application.Chat.ViewModels;
+
+namespace Application.Email.Base;
+
+public partial class EmailViewModel : ViewModel
+{
+    //Fields
+    public ChatViewModel ChildViewModel { get; private set; }
+
+    //Constructiom
+    public EmailViewModel(ChatViewModel chatViewModel)
+    {
+        ChildViewModel = chatViewModel;
+    }
+}
