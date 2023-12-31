95b96f26ea16ffb9e32e27c6e824f0ada0be9d7e
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-12-31 12:36:12 +0200

    Update ViewModel.cs

--- src/Core/Application/.Common/.Base/ViewModel.cs
+++ src/Core/Application/.Common/.Base/ViewModel.cs
@@ -1,7 +1,18 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Application.Email.Base;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Diagnostics;
 
 namespace Application.Common.Base;
 
 public abstract partial class ViewModel : ObservableObject
 {
+    //ViewModel lifecylce
+    public virtual void OnViewModelStarting(CancellationToken cancellationToken = default)
+    {
+        Debug.WriteLine($"{nameof(EmailViewModel)} is starting");
+    }
+    public virtual void OnViewModelClosing(CancellationToken cancellationToken = default)
+    {
+        Debug.WriteLine($"{nameof(EmailViewModel)} is closing");
+    }
 }
