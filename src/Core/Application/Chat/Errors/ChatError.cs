05bb6dcf5a1796789621df5400b26ca7d2c97d1c
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2024-01-13 20:46:39 +0200

    Create ChatError.cs

---
+++ src/Core/Application/Chat/Errors/ChatError.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Chat.Errors;
+
+public class ChatError : AppError
+{
+    public ChatError(string value, string description) : base(value, description)
+    {
+    }
+
+    public static readonly AppError PermissionError = new("PermissionError", "Permission not granted to use device microphone.");
+    public static readonly AppError RecognitionFailedError = new("SpeechRecognitionFailed", "Speech recognition has failed!");
+}
