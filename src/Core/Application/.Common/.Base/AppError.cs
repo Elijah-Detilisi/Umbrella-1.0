ebe16bdc6b0cedb190271c1c8fecdd3d0b4e40bb
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2024-01-13 20:40:22 +0200

    Update AppError.cs

---
+++ src/Core/Application/.Common/.Base/AppError.cs
@@ -0,0 +1,21 @@
+﻿namespace Application.Common.Base;
+
+public class AppError
+{
+    //Properties
+    public string Value { get; set; }
+    public string Description { get; set; }
+
+    //Construction
+    public AppError(string value, string description)
+    {
+        Value = value;
+        Description = description;
+    }
+
+    //Standard
+    public static readonly AppError Crash = new("Crash", "Application crashed!");
+    public static readonly AppError Error = new("Error", "An error has occured.");
+    public static readonly AppError Cancelled = new("Cancelled", "The operation was cancelled."); 
+    
+}
