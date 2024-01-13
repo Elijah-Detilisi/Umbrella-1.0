88ce4ae2ad225182cadc7f599089a4f6f0fcc739
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2024-01-13 21:20:56 +0200

    Update EmailListingViewModel.cs

--- src/Core/Application/Email/ViewModels/EmailListingViewModel.cs
+++ src/Core/Application/Email/ViewModels/EmailListingViewModel.cs
@@ -8,4 +8,13 @@ public class EmailListingViewModel : EmailViewModel
     public EmailListingViewModel(ChatViewModel chatViewModel) : base(chatViewModel)
     {
     }
+
+    //Keep list of emails in inbox.
+    //Populate list with lastest emails.
+    //Commands to view an email.
+    //Command to delete an email.
+    //Command to right an email
+    //Command to search for an email
+    //Command to get latest email
+    //Command to read through all emails
 }
