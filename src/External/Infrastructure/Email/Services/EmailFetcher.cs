efe25a4047b7bfba01e40ba603bd12231097ebc3
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2024-01-18 20:45:19 +0200

    Create EmailFetcher.cs

---
+++ src/External/Infrastructure/Email/Services/EmailFetcher.cs
@@ -0,0 +1,12 @@
+﻿using Application.Email.Models;
+using Application.Email.Services;
+
+namespace Infrastructure.Email.Services;
+
+public class EmailFetcher : IEmailFetcher
+{
+    Task<List<EmailModel>> IEmailFetcher.GetEmailsAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
