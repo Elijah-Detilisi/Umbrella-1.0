5f84fd8536109d24575e793a34d5724377976edc
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2024-01-18 21:52:33 +0200

    Update GlobalUsings.cs

--- src/External/Infrastructure/GlobalUsings.cs
+++ src/External/Infrastructure/GlobalUsings.cs
@@ -1,5 +1,16 @@
-﻿global using Application.Common.Services;
-global using CommunityToolkit.Maui.Media;
-global using System.Globalization;
+﻿global using Domain.Common.ValueObjects;
+global using Domain.Email.Entities.Enums;
+global using Domain.Email.ValueObjects;
+
+global using Application.Email.Models;
+global using Application.Email.Services;
+global using Application.User.Models;
+global using Application.Common.Services;
+
+global using MimeKit;
 global using MailKit.Net.Smtp;
 global using MailKit.Net.Pop3;
+
+global using System.Globalization;
+global using CommunityToolkit.Maui.Media;
+global using Infrastructure.Email.Settings;
