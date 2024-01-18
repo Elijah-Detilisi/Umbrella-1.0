2238d439392439e5f30a64796ffcad135ee1df75
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2024-01-18 20:59:22 +0200

    Update GlobalUsings.cs

--- src/External/Infrastructure/GlobalUsings.cs
+++ src/External/Infrastructure/GlobalUsings.cs
@@ -1,3 +1,5 @@
 ﻿global using Application.Common.Services;
 global using CommunityToolkit.Maui.Media;
 global using System.Globalization;
+global using MailKit.Net.Smtp;
+global using MailKit.Net.Pop3;
