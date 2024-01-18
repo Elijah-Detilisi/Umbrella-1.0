fb3c56f8ba1fe7a92653f60fda7447b92ec9b8b2
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2024-01-18 21:25:33 +0200

    Create Pop3ServerSettings.cs

---
+++ src/External/Infrastructure/Email/.Base/ServerSettings.cs
@@ -0,0 +1,19 @@
+﻿namespace Infrastructure.Email.Base;
+
+public class ServerSettings
+{
+    //Properties
+    public string Provider { get; set; }
+    public string Server { get; set; }
+    public int Port { get; set; }
+    public bool UseSsl { get; set; }
+
+    //Construction
+    public ServerSettings(string provider, string server, int port, bool useSsl)
+    {
+        Provider = provider;
+        Server = server;
+        Port = port;
+        UseSsl = useSsl;
+    }
+}
