7d6d6f8aa9b3e3b9e6d94160bc152ad63443f125
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2024-01-19 19:56:19 +0200

    Update IEmailFetcher.cs

--- src/Core/Application/Email/Services/IEmailFetcher.cs
+++ src/Core/Application/Email/Services/IEmailFetcher.cs
@@ -2,7 +2,8 @@
 
 namespace Application.Email.Services;
 
-public interface IEmailFetcher
+public interface IEmailFetcher : IDisposable
 {
+    bool IsConnected { get; }
     List<EmailModel> GetEmailsAsync(CancellationToken cancellationToken);
 }
