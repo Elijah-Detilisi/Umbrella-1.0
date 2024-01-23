8b69687aaccdf8557884cfa3491e4ef018ecb784
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2024-01-23 21:46:01 +0200

    Create TextExtensions.cs

---
+++ src/External/Infrastructure/Email/Extensions/TextExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Infrastructure.Email.Extensions;
+
+public static class TextExtensions
+{
+    public static string ShortText(this string text)
+    {
+        if (!string.IsNullOrEmpty(text))
+        {
+            text = text.Trim();
+            text = EnsureSingleNewline(text);
+            text = ShortenUrlsInText(text);
+        }
+
+        return text;
+    }
+
+    //Helper function
+    private static string EnsureSingleNewline(string input)
+    {
+        // Use regular expression to replace consecutive newline characters with a single newline
+        var result = Regex.Replace(input, @"\n+", "\n");
+
+        return result;
+    }
+
+    private static string ShortenUrlsInText(string input)
+    {
+        // Define a regular expression pattern to match URLs
+        string urlPattern = @"(https?://\S+)";
+
+        // Use regex to find all matches
+        MatchCollection matches = Regex.Matches(input, urlPattern);
+
+        // Iterate through matches and shorten URLs
+        foreach (Match match in matches.Cast<Match>())
+        {
+            string originalUrl = match.Groups[1].Value;
+            string shortenedUrl = originalUrl.Length > 30 ? originalUrl[..30] + "..." : originalUrl;
+            input = input.Replace(originalUrl, shortenedUrl);
+        }
+
+        return input;
+    }
+}
