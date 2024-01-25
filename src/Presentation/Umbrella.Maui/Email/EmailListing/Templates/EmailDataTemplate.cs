d1347a3151a7392d82e9af3de952a598a7ec1b34
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2024-01-25 22:33:04 +0200

    Create EmailDataTemplate.cs

---
+++ src/Presentation/Umbrella.Maui/Email/EmailListing/Templates/EmailDataTemplate.cs
@@ -0,0 +1,148 @@
+﻿namespace Umbrella.Maui.Email.EmailListing.Templates;
+
+public class EmailDataTemplate : DataTemplate
+{
+    //Fields
+    private enum Column { Left = 0, Right = 1 }
+
+    //View components 
+    private static Grid? MainGrid;
+    private static Image? ImageIcon;
+    private static Grid? ContentGrid;
+    private static Label? EmailTimeLabel;
+    private static Label? EmailSenderLabel;
+    private static Label? EmailSubjectLabel;
+    private static VerticalStackLayout? EmailTimeLayout;
+    private static VerticalStackLayout? EmailDetailsLayout;
+
+    
+    //Construction
+    public EmailDataTemplate() : base(CreateTemplateGrid)
+    {
+
+    }
+
+    //Initialization
+    private static Grid? CreateTemplateGrid()
+    {
+        InitializeImageIcon();
+        InitializeEmailLabels();
+        InitializeEmailDetailsLayout();
+        InitializeEmailTimeLayout();
+        InitializeMainGrid(); // NB: Must be last to initialize
+
+        return MainGrid;
+    }
+
+    //View component Initialization 
+    private static void InitializeImageIcon()
+    {
+        ImageIcon = new()
+        {
+            WidthRequest = 30,
+            HeightRequest = 30,
+            Source = "envelope_solid.svg"
+        };
+    }
+
+    private static void InitializeEmailLabels()
+    {
+        //Setup
+        EmailTimeLabel = new() {
+            FontSize = 14 
+        };
+        
+        EmailSubjectLabel = new()
+        {
+            MaxLines = 1, FontSize = 12,
+            LineBreakMode = LineBreakMode.WordWrap
+        };
+
+        EmailSenderLabel = new()
+        {
+            MaxLines = 1, FontSize = 16,
+            FontAttributes = FontAttributes.Bold,
+            LineBreakMode = LineBreakMode.TailTruncation
+        };
+
+        //Databings
+        EmailTimeLabel.Bind(Label.TextProperty,
+            static (EmailModel email) => email.CreatedAt, mode: BindingMode.OneTime, 
+            stringFormat: "{}{0:h:mm tt}"
+        );
+
+        EmailSubjectLabel.Bind(Label.TextProperty,
+            static (EmailModel email) => email.Subject.Value, mode: BindingMode.OneTime
+        );
+
+        EmailSenderLabel.Bind(Label.TextProperty,
+            static (EmailModel email) => email.Sender.Value, mode: BindingMode.OneTime
+        );
+    }
+
+    private static void InitializeEmailDetailsLayout()
+    {
+        EmailDetailsLayout = new()
+        {
+            Spacing = 5,
+            Children =
+            {
+                EmailSenderLabel, 
+                EmailSubjectLabel
+            }
+        };
+    }
+
+    private static void InitializeEmailTimeLayout()
+    {
+        EmailTimeLayout = new()
+        {
+            Spacing = 5,
+            Children =
+            {
+                EmailTimeLabel
+            }
+        };
+    }
+
+    private static void InitializeContentGrid()
+    {
+        var leftColumnSize = 1;
+        var rightColumnSize = 0;
+
+        ContentGrid = new()
+        {
+            ColumnDefinitions = 
+            [
+                new() { Width = new GridLength(leftColumnSize, GridUnitType.Star) },
+                new (){ Width = new GridLength(rightColumnSize, GridUnitType.Auto) }
+            ],
+            Children =
+            {
+                EmailDetailsLayout?.Column(Column.Left),
+                EmailTimeLayout?.Column(Column.Right)
+            }
+        };
+    }
+
+    private static void InitializeMainGrid()
+    {
+        var leftColumnSize = 0;
+        var rightColumnSize = 1;
+
+        MainGrid = new()
+        {
+            ColumnSpacing = 15,
+            ColumnDefinitions = 
+            [
+                new() { Width = new GridLength(leftColumnSize, GridUnitType.Auto) },
+                new() { Width = new GridLength(rightColumnSize, GridUnitType.Star) }
+            ],
+            Children =
+            {
+                ImageIcon?.Column(Column.Left),
+                ContentGrid?.Column(Column.Right)
+            }
+        };
+    }
+}
