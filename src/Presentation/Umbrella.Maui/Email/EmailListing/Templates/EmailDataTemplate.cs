ba449f40c9581febb204ff899ba0094a10155853
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2024-01-27 09:52:03 +0200

    Create EmailDataTemplate1.cs

--- src/Presentation/Umbrella.Maui/Email/EmailListing/Templates/EmailDataTemplate.cs
+++ src/Presentation/Umbrella.Maui/Email/EmailListing/Templates/EmailDataTemplate.cs
@@ -1,22 +1,22 @@
-﻿namespace Umbrella.Maui.Email.EmailListing.Templates;
+﻿using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Maui.Layouts;
+
+namespace Umbrella.Maui.Email.EmailListing.Templates;
 
 public class EmailDataTemplate : DataTemplate
 {
     //Fields
-    private enum Column { Left = 0, Right = 1 }
+    private enum Column { Left = 0, Center = 1, Right = 2 }
 
-    //View components 
-    private static Grid? MainGrid;
+    //View components
     private static Image? ImageIcon;
     private static Grid? ContentGrid;
     private static Label? EmailTimeLabel;
     private static Label? EmailSenderLabel;
     private static Label? EmailSubjectLabel;
     private static BoxView? SeparaterBoxView;
-    private static VerticalStackLayout? MainLayout;
-    private static VerticalStackLayout? EmailTimeLayout;
     private static VerticalStackLayout? EmailDetailsLayout;
-    
+
     //Construction
     public EmailDataTemplate() : base(CreateTemplate)
     {
@@ -24,17 +24,23 @@ public class EmailDataTemplate : DataTemplate
     }
 
     //Initialization
-    private static VerticalStackLayout? CreateTemplate()
+    private static DockLayout? CreateTemplate()
     {
         InitializeImageIcon();
         InitializeSeparaterBoxView();
         InitializeEmailLabels();
         InitializeEmailDetailsLayout();
-        InitializeEmailTimeLayout();
-        InitializeMainGrid();
-        InitializeMainLayout();// NB: Must be last to initialize
+        InitializeContentGrid();
 
-        return MainLayout;
+        return new()
+        {
+            Padding = new Thickness(10),
+            Children =
+            {
+                ContentGrid,
+                SeparaterBoxView
+            }
+        };
     }
 
     //View component Initialization 
@@ -42,8 +48,8 @@ public class EmailDataTemplate : DataTemplate
     {
         ImageIcon = new()
         {
-            WidthRequest = 30,
-            HeightRequest = 30,
+            WidthRequest = 50,
+            HeightRequest = 50,
             Source = "envelope_solid.svg"
         };
     }
@@ -56,32 +62,36 @@ public class EmailDataTemplate : DataTemplate
             HorizontalOptions = LayoutOptions.Fill
         };
 
+        DockLayout.SetDockPosition(SeparaterBoxView, DockPosition.Bottom);
         SeparaterBoxView.DynamicResource(View.BackgroundColorProperty, "AppActionColor");
     }
     private static void InitializeEmailLabels()
     {
         //Setup
-        EmailTimeLabel = new() {
-            FontSize = 14 
+        EmailTimeLabel = new()
+        {
+            FontSize = 14
         };
-        
+
         EmailSubjectLabel = new()
         {
-            MaxLines = 1, FontSize = 12,
+            MaxLines = 1,
+            FontSize = 12,
             LineBreakMode = LineBreakMode.WordWrap
         };
 
         EmailSenderLabel = new()
         {
-            MaxLines = 1, FontSize = 16,
+            MaxLines = 1,
+            FontSize = 16,
             FontAttributes = FontAttributes.Bold,
-            LineBreakMode = LineBreakMode.TailTruncation
+            LineBreakMode = LineBreakMode.TailTruncation,
         };
 
         //Databings
         EmailTimeLabel.Bind(Label.TextProperty,
-            static (EmailModel email) => email.CreatedAt, mode: BindingMode.OneTime, 
-            stringFormat: "{}{0:h:mm tt}"
+            static (EmailModel email) => email.CreatedAt, mode: BindingMode.OneTime,
+            stringFormat: "{0:h:mm tt}"
         );
 
         EmailSubjectLabel.Bind(Label.TextProperty,
@@ -92,7 +102,6 @@ public class EmailDataTemplate : DataTemplate
             static (EmailModel email) => email.Sender.Value, mode: BindingMode.OneTime
         );
     }
-
     private static void InitializeEmailDetailsLayout()
     {
         EmailDetailsLayout = new()
@@ -100,74 +109,32 @@ public class EmailDataTemplate : DataTemplate
             Spacing = 5,
             Children =
             {
-                EmailSenderLabel, 
+                EmailSenderLabel,
                 EmailSubjectLabel
             }
         };
-    }
 
-    private static void InitializeEmailTimeLayout()
-    {
-        EmailTimeLayout = new()
-        {
-            Spacing = 5,
-            Children =
-            {
-                EmailTimeLabel
-            }
-        };
+        DockLayout.SetDockPosition(EmailDetailsLayout, DockPosition.Top);
     }
-
     private static void InitializeContentGrid()
     {
-        var leftColumnSize = 1;
-        var rightColumnSize = 0;
+        var centerColumn = 0.6;
+        var lateralColumn = 0.2;
 
         ContentGrid = new()
         {
-            ColumnDefinitions = 
+            ColumnSpacing = 5,
+            ColumnDefinitions =
             [
-                new() { Width = new GridLength(leftColumnSize, GridUnitType.Star) },
-                new (){ Width = new GridLength(rightColumnSize, GridUnitType.Auto) }
-            ],
-            Children =
-            {
-                EmailDetailsLayout?.Column(Column.Left),
-                EmailTimeLayout?.Column(Column.Right)
-            }
-        };
-    }
-
-    private static void InitializeMainGrid()
-    {
-        var leftColumnSize = 0;
-        var rightColumnSize = 1;
-
-        MainGrid = new()
-        {
-            ColumnSpacing = 15,
-            ColumnDefinitions = 
-            [
-                new() { Width = new GridLength(leftColumnSize, GridUnitType.Auto) },
-                new() { Width = new GridLength(rightColumnSize, GridUnitType.Star) }
+                new() { Width = new GridLength(lateralColumn, GridUnitType.Star) },
+                new() { Width = new GridLength(centerColumn, GridUnitType.Star) },
+                new() { Width = new GridLength(lateralColumn, GridUnitType.Star) },
             ],
             Children =
             {
                 ImageIcon?.Column(Column.Left),
-                ContentGrid?.Column(Column.Right)
-            }
-        };
-    }
-
-    private static void InitializeMainLayout()
-    {
-        MainLayout = new()
-        {
-            Padding = new Thickness(10),
-            Children =
-            {
-                MainGrid,
-                SeparaterBoxView
+                EmailDetailsLayout?.Column(Column.Center),
+                EmailTimeLabel?.Column(Column.Right),
             }
         };
     }
