af433644509392a7b9e2b064836f746e6148b39e
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2024-02-10 17:03:00 +0200

    Update AppShell.xaml.cs

--- src/Presentation/Umbrella.Maui/AppShell.xaml.cs
+++ src/Presentation/Umbrella.Maui/AppShell.xaml.cs
@@ -1,10 +1,16 @@
-﻿namespace Umbrella.Maui
+﻿using Umbrella.Maui.Email.EmailDetail.Pages;
+using Umbrella.Maui.Email.EmailListing.Pages;
+
+namespace Umbrella.Maui
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+
+            Routing.RegisterRoute(nameof(EmailListingPage), typeof(EmailListingPage));
+            Routing.RegisterRoute(nameof(EmailDetailPage), typeof(EmailDetailPage));
         }
     }
 }
