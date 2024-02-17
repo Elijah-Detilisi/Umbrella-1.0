8a255954b4ad75c1fbbb2ed89654461ab33a30f6
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2024-02-17 14:22:23 +0200

    Create AppFileService.cs

---
+++ src/External/Infrastructure/.Common/Service/AppFileService.cs
@@ -0,0 +1,9 @@
+﻿namespace Infrastructure.Common.Service;
+
+public class AppFileService : IAppFileService
+{
+    public string GetDatabasePath(string filename)
+    {
+        return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), filename);
+    }
+}
